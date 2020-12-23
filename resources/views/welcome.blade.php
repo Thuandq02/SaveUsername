b0fc0718c0505488ff3783b3bf15bd858d02bce3
dqthua <duongquangthuan2002@gmail.com>
2020-12-23 11:41:17 +0700

    them

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('content')
+{{--    <div class="title m-b-md">--}}
+{{--        Sử dụng Session đếm số lượng lượt xem--}}
+{{--    </div>--}}
+
+    <div class="links">
+        <button><h1><a href="{{ route('show.login') }}">Login</a></h1></button>
+    </div>
+@endsection
